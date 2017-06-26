4af410aa4bf798877dee5539710442d59af7f11f
behnam2019 <behnam2019@gmail.com>
2017-06-26 01:14:52 -0700

    Update ar.js

--- www/js/ar.js
+++ www/js/ar.js
@@ -8,8 +8,8 @@ var World = {
 
   // Determine Overlay
     createOverlays: function createOverlaysFn() {
-        this.tracker = new AR.ClientTracker("assets/magazine.wtc");
-        var imgOne = new AR.ImageResource("assets/imageOne.png");
+        this.tracker = new AR.ClientTracker("www/img/magazine.wtc");
+        var imgOne = new AR.ImageResource("www/img/imageOne.png");
         var overlayOne = new AR.ImageDrawable(imgOne, 1, {
             offsetX: -0.15,
             offsetY: 0
