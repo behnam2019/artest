3a5ff3c9e532db75d68ff7ec35236d3fa1384ef4
behnam2019 <behnam2019@gmail.com>
2017-06-26 01:10:15 -0700

    Create ar.js

---
+++ www/js/ar.js
@@ -0,0 +1,26 @@
+var World = {
+    loaded: false,
+
+  // Initialize the process
+    init: function initFn() {
+        this.createOverlays();
+    },
+
+  // Determine Overlay
+    createOverlays: function createOverlaysFn() {
+        this.tracker = new AR.ClientTracker("assets/magazine.wtc");
+        var imgOne = new AR.ImageResource("assets/imageOne.png");
+        var overlayOne = new AR.ImageDrawable(imgOne, 1, {
+            offsetX: -0.15,
+            offsetY: 0
+        });
+        var pageOne = new AR.Trackable2DObject(this.tracker, "*", {
+            drawables: {
+                cam: overlayOne
+            }
+        });
+    }
+
+};
+
+World.init();
