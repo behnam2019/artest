ca4eecd36ce1755fcac55d13988a2574ccbf8c66
behnam2019 <behnam2019@gmail.com>
2017-06-26 01:08:52 -0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,105 +1,45 @@
-
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+  // initialization
+  initialize: function() {
+    this.bindEvents();
+  },
+    // set event
+  bindEvents: function() {
+    document.addEventListener('deviceready', app.onDeviceReady, false);
+  },
 
-app.wikitudePlugin = cordova.require("com.wikitude.phonegap.WikitudePlugin.WikitudePlugin")
-//var worldpath = cordova.file.dataDirectory + 'www/index.html';
+  onDeviceReady: function() {
+    wikitudePlugin.isDeviceSupported(
+      app.onDeviceSupported,
+        app.onDeviceNotSupported,
+        [ "2d_tracking"]
+    );
+  },
 
-var world = {
-            "worldpath": "www/index.html",
-            "requiredFeatures": [
-                "2d_tracking",
-                "geo"
-            ],
-            "startupConfiguration": {
-                "camera_position": "back"
-            }
-        };
-        app.loadARchitectWorld(world);
+  // run on AR supported Devices
+  onDeviceSupported: function() {
+    wikitudePlugin.loadARchitectWorld(
+      app.onARExperienceLoadedSuccessful, 
+      app.onARExperienceLoadError,
+      "www/ar.html",
+      [ "2d_tracking"],
+      {"camera_position": "back"}
+    );
+  },
 
+  // if device isnt supported, show error
+  onDeviceNotSupported: function(errorMessage) {
+    alert(errorMessage);
+  },
 
-//loadCustomARchitectWorldFromURL(cordova.file.dataDirectory + 'www/index.html');
+  // if AR hass been loaded successfully
+  onARExperienceLoadedSuccessful: function(loadedURL) {
+  },
 
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    },
-
-loadARchitectWorld: function(architectWorld){
-
-app.wikitudePlugin.isDeviceSupported(function() {
-
-
-app.wikitudePlugin.setOnUrlInvokeCallback(app.onUrlInvoke);
-app.wikitudePlugin.loadARchitectWorld(function successFn(loadedURL) {
-
-}, function errorFn(error) {
-alert('Loading AR web view failed: ' + error);
-},
-architectWorld.path, architectWorld.requiredFeatures, architectWorld.startupConfiguration
-
-);
-}, function(errorMessage) {
-alert('device suport nemikonad');
-alert(errorMessage)
-},
-architectWorld.requiredFeatures
-);
-
-},
-
-loadCustomARchitectWorldFromURL: function(url) {
-        var world = {
-            "worldpath": url,
-            "requiredFeatures": [
-                "2d_tracking",
-                "geo"
-            ],
-            "startupConfiguration": {
-                "camera_position": "back"
-            }
-        };
-        app.loadARchitectWorld(world);
-    },
-
-
-onUrlInvoke: function (url) {
-if (url.indexOf('captureScreen') > -1) {
-
-}
-else{
-
-    alert(url + "not handled");
-
-}
-}
-
-
-
-
+  // if AR has not been loaded, show error message
+  onARExperienceLoadError: function(errorMessage) {
+    alert('Loading AR web view failed: ' + errorMessage);
+  }
 };
 
+app.initialize();
